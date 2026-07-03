5f7bbb5cfc7fa5a0daf81bd25010eb37ee92ef07
Victor Mendonça <vhugolm@gmail.com>
2026-07-02 21:22:40 -0300

    cap4 primeiro

---
+++ src/main/java/com/mycompany/deitel/java/cap9/BasePlusCommissionEmployeeComExtends.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.deitel.java.cap9;
+
+/**
+ *
+ * @author victormendonca
+ */
+public class BasePlusCommissionEmployeeComExtends extends CommissionEmployee{
+    
+    private double baseSalary;
+    
+    public BasePlusCommissionEmployeeComExtends(String firstName, String lastName, 
+            String socialSecurityNumber, double grossSales,double commissionRate, double baseSalary)
+    {
+        super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
+        
+        if(baseSalary < 0.0)
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+        
+        this.baseSalary = baseSalary;
+    }
+    
+    public void setBaseSalary(double baseSalary)
+    {
+        if(baseSalary < 0.0)
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+        
+        this.baseSalary = baseSalary;
+    }
+    
+    public double getBaseSalary()
+    {
+        return baseSalary;
+    }
+    
+    @Override
+    public double earnings()
+    {
+        return getBaseSalary() + super.earnings();
+    }
+    
+    @Override
+    public String toString()
+    {
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: %.2f", 
+                "base-salaried commission employee", super.getFirstName(), super.getLastName(),
+                "social security number", super.getSocialSecurityNumber(),
+                "gross sales", super.getGrossSales(), "commission rate", super.getCommissionRate(),
+                "base salary", baseSalary);
+    }   
+}
---
+++ src/main/java/com/mycompany/deitel/java/cap9/BasePlusCommissionEmployeeTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.deitel.java.cap9;
+
+/**
+ *
+ * @author victormendonca
+ */
+public class BasePlusCommissionEmployeeTest {
+    public static void main(String[] args) {
+        var employee = new BasePlusCommissionEmployee("Bob", "Lewis", "333-33-3333", 5000, .04, 300);
+        
+        System.out.println("Employee information obtained by get methods:%n");
+        System.out.printf("%s %s%n", "First name is", employee.getFirstName());
+        System.out.printf("%s %s%n", "Last name is", employee.getLastName());
+        System.out.printf("%s %s%n", "Social security number is", employee.getSocialSecurityNumber());
+        System.out.printf("%s %s%n", "Gross sales is", employee.getGrossSales());
+        System.out.printf("%s %.2f%n", "Commission rate is", employee.getCommissionRate());
+        System.out.printf("%s %.2f%n", "Base salary is", employee.getBaseSalary());
+        
+        employee.setBaseSalary(1000);
+        System.out.printf("%n%s:%n%n%s%n", "Update employee information obtained by toString", employee.toString());
+        System.out.println("%n-----------------%n%n");
+        BasePlusCommissionEmployeeComExtends novoEmployee = 
+                                new BasePlusCommissionEmployeeComExtends("Bob", "Lewis", "333-33-3333", 5000.0, .04, 300.0);
+        
+        System.out.println("novoEmployee information obtained by get methods:%n");
+        System.out.printf("%s %s%n", "First name is", novoEmployee.getFirstName());
+        System.out.printf("%s %s%n", "Last name is", novoEmployee.getLastName());
+        System.out.printf("%s %s%n", "Social security number is", novoEmployee.getSocialSecurityNumber());
+        System.out.printf("%s %s%n", "Gross sales is", novoEmployee.getGrossSales());
+        System.out.printf("%s %.2f%n", "Commission rate is", novoEmployee.getCommissionRate());
+        System.out.printf("%s %.2f%n", "Base salary is", novoEmployee.getBaseSalary());
+        System.out.printf("%s %.2f%n", "earnings", novoEmployee.earnings());
+        
+        System.out.printf("%n%s%n", novoEmployee.toString());
+        
+    }
+    
+}
